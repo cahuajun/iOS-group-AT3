305366caee2b4bd0014577463f80deb5ff6510f8
b0rk-finance <elena.g.okeeffe@student.uts.edu.au>
2025-05-07 18:10:44 +1000

    Update ContentView.swift
    Add test parking spots

--- iOS-group-AT3/ContentView.swift
+++ iOS-group-AT3/ContentView.swift
@@ -11,9 +11,11 @@ import MapKit
 struct ContentView: View {
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
 
-    var body: some View {
-        Map(coordinateRegion: $region)
-            .frame(width: 400, height: 300)
+    Map{
+        Marker("Parking 1", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275))
+        Marker("Parking 2", coordinate: CLLocationCoordinate2D(latitude: 51.537222, longitude: -0.1275))
+        Marker("Parking 3", coordinate: CLLocationCoordinate2D(latitude: 51.537222, longitude: -0.1475))
+    }.frame(width: 400, height: 300)
     }
 }
 
