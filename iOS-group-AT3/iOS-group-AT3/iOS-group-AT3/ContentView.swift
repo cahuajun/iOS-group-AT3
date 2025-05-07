a904e89431563eade92e2f7545808f9ae45bcf57
b0rk-finance <elena.g.okeeffe@student.uts.edu.au>
2025-05-07 17:54:51 +1000

    Add files via upload

---
+++ iOS-group-AT3/iOS-group-AT3/iOS-group-AT3/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  iOS-group-AT3
+//
+//  Created by Elena O'Keeffe on 7/5/2025.
+//
+
+import SwiftUI
+import MapKit
+
+struct ContentView: View {
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+
+    var body: some View {
+        Map(coordinateRegion: $region)
+            .frame(width: 400, height: 300)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS-group-AT3/iOS-group-AT3/iOS-group-AT3/iOS_group_AT3App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_group_AT3App.swift
+//  iOS-group-AT3
+//
+//  Created by Elena O'Keeffe on 7/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct iOS_group_AT3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
