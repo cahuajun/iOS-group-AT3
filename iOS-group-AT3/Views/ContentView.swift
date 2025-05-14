7c4c89942a51eb534620168f85d05621f56dc4e6
eokeeffe <elena.g.okeeffe@student.uts.edu.au>
2025-05-14 15:45:51 +1000

    add comments to contentView

--- iOS-group-AT3/Views/ContentView.swift
+++ iOS-group-AT3/Views/ContentView.swift
@@ -9,17 +9,31 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
-    @State private var isActive = false
-    @State private var selectedLocation = CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275)
+    
+    /**
+     Present the user with a map of available parking spots
+     Each parking spot is represented by a clickable pin
+     */
     @State private var startPosition = MapCameraPosition.region(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: -33.882889, longitude: 151.199611), span: MKCoordinateSpan(latitudeDelta: 0.008, longitudeDelta: 0.008)))
     
+    /**
+     Selection allows the correct details for the selected pin to be loaded in the detail view
+     */
     @State private var selection: ParkingSpot? = nil
+    
+    /**
+     A list of available parking spots which is populated from a json resource onAppear of the Map
+     */
     @State var allSpots: [ParkingSpot]
-    var showHistory: Bool = true
     
     var body: some View {
         VStack{
             NavigationStack{
+                /**
+                 Present a link to the user's parking history
+                 For demonstration purposes this is loaded from a .json resource
+                 If this application was to be further developed these should come from a database
+                 */
                 NavigationLink {
                     ParkingHistoryView()
                 } label: {
@@ -32,32 +46,49 @@ struct ContentView: View {
                 .padding()
                 
                 ZStack{
-                    Map(initialPosition:  startPosition, interactionModes: .all, selection: $selection) {
+                    /**
+                     A map of recorded parking spots
+                     This map initially shows the UTS area parking spots are shown on the map as pins
+                     */
+                    Map(initialPosition:  startPosition,
+                        /**
+                         Allow all interaction modes including pan, zoom, pitch and rotate
+                         */
+                        interactionModes: .all,
+                        
+                        selection: $selection) {
                         ForEach(allSpots) { spot in
-                            Marker(spot.name, coordinate: CLLocationCoordinate2D(latitude: spot.lat, longitude: spot.long))
+                            Marker(spot.name,
+                                   coordinate: CLLocationCoordinate2D(latitude: spot.lat, longitude: spot.long))
                                 .tag(spot)
                         }
                     }
                     .sheet(item: $selection) { spot in
+                        /**
+                         This sheet slides up from the bottom of the screen to show the details of the selected parking spot
+                         */
                         NavigationStack {
                             DetailView(parkingSpot: spot)
                         }
+                        /**
+                         The details view occupies approximately half of the screen
+                         Details view can be dismissed by clicking on the map
+                         */
                         .presentationDetents([.medium])
-                    }.onAppear {
-                        loadJSONData()
+                    }
+                    .onAppear {
+                        /**
+                         Initialise parking spots from .json resource for demo
+                         To productionise this we would implement a connection to a database
+                         */
+                        allSpots = Utility.loadParkingSpots()
                     }
                 }
-                
             }
         }
-    }
-    func loadJSONData() {
-        allSpots = Utility.loadParkingSpots()
     }
 }
 
 #Preview {
-    ContentView(allSpots:[]
-    
-    )
+    ContentView(allSpots:[])
 }
