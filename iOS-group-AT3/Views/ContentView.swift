efe747105b70508eaf6ee1a8043798eb330c3373
Elena <elena.g.okeeffe@student.uts.edu.au>
2025-05-14 12:42:52 +1000

    fix map scale

--- iOS-group-AT3/Views/ContentView.swift
+++ iOS-group-AT3/Views/ContentView.swift
@@ -11,7 +11,7 @@ import MapKit
 struct ContentView: View {
     @State private var isActive = false
     @State private var selectedLocation = CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275)
-    @State private var startPosition = MapCameraPosition.region(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: -33.882889, longitude: 151.199611), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)))
+    @State private var startPosition = MapCameraPosition.region(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: -33.882889, longitude: 151.199611), span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)))
     
     @State private var selection: ParkingSpot? = nil
     @State var allSpots: [ParkingSpot]
@@ -53,55 +53,7 @@ struct ContentView: View {
 
 
 #Preview {
-    ContentView(allSpots:
-                [ParkingSpot( id: "T1",
-                                 name: "Thomas Street 1",
-                                 description: "Two-storey parking lot",
-                                 imageName: "image1",
-                                 rating: 4.5,
-                                 lat: -33.882889,
-                                 long: 151.199611,
-                                 count: 5,
-                                 date: Date(),
-                                 comments:[]),
-                 ParkingSpot(
-                   id: "M1",
-                   name: "Mary Ann Street 1",
-                   description: "Near the UTS campus",
-                   imageName: "image2",
-                   rating: 4.0,
-                   lat: -33.881278,
-                   long: 151.199083,
-                   count: 2,
-                   date: Date(),
-                   comments:[]
-                 ),
-                 ParkingSpot(
-                   id: "B1",
-                   name: "Blackfriars Pi 1",
-                   description: "Solely for students",
-                   imageName: "image3",
-                   rating: 3.5,
-                   lat: -33.885639,
-                   long: 151.198083,
-                   count: 1,
-                   date: Date(),
-                   comments: []
-                 ),
-                 ParkingSpot(
-                   id: "W1",
-                   name: "Wattle Street 1",
-                   description: "Only two, sometimes depends on the lucky",
-                   imageName: "image2",
-                   rating: 4.2,
-                   lat: -33.882083,
-                   long: 151.197528,
-                   count: 2,
-                   date: Date(),
-                   comments:[]
-                 ),
-                
-                ]
+    ContentView(allSpots:[]
     
     )
 }
