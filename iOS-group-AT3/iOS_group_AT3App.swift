f48da3a220417adb0617e6b9f4833d411ac4e013
eokeeffe <elena.g.okeeffe@student.uts.edu.au>
2025-05-14 13:57:57 +1000

    refactor

--- iOS-group-AT3/iOS_group_AT3App.swift
+++ iOS-group-AT3/iOS_group_AT3App.swift
@@ -12,26 +12,8 @@ struct iOS_group_AT3App: App {
     
 
     var body: some Scene {
-            var allSport: [ParkingSpot] = loadJSONData()
             WindowGroup {
-                ContentView(allSpots: allSport)
+                ContentView(allSpots: [])
             }
-    }
-    
-    func loadJSONData() -> [ParkingSpot]{
-        guard let url = Bundle.main.url(forResource: "parking_history", withExtension: "json") else {
-            print("❌ parking_history.json not found")
-            return []
-        }
-
-        do {
-            let data = try Data(contentsOf: url)
-            let decoder = JSONDecoder()
-            decoder.dateDecodingStrategy = .iso8601
-            return try decoder.decode([ParkingSpot].self, from: data)
-        } catch {
-            print("❌ Failed to decode parking_history.json: \(error)")
-            return []
-        }
     }
 }
